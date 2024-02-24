404ff35531be5934ef8fe4c90595f02e41f77d4a
Ana <Ana@192.168.0.5>
2024-02-24 19:09:54 -0300

    SomaCommit

---
+++ ex01/src/ex01/somaas.java
@@ -0,0 +1,23 @@
+package ex01;
+import java.util.*;
+
+
+public class somaas {
+	public static Scanner sc= new Scanner(System.in);
+	
+public static void main(String args[]) {
+	
+	int num1,num2,soma;
+	
+	System.out.print("Digite um número:");
+	num1=sc.nextInt();
+	
+	System.out.print("Digite outro número:");
+	num2=sc.nextInt();
+	
+	soma=num1+num2;
+	
+	System.out.print("Soma:"+soma);
+}
+}
+
